49f86e75ae102a9da0806f0fd95ae455209f0e20
Ekasit Kijsipongse <ekasit.kijsipongse@nectec.or.th>
2016-01-20 14:35:30 +0700

    Add more example code

--- src/org/ganglia/client4j/example/TestSimpleGangliaClient.java
+++ src/org/ganglia/client4j/example/TestSimpleGangliaClient.java
@@ -7,13 +7,23 @@ public class TestSimpleGangliaClient {
       String GANGLIA_GMETAD_HOST = "localhost";
       String CLUSTER_NAME = "spark-paas-dev";
 
-      System.out.println("Testing non-interactive port");
+      System.out.println("Testing non-interactive port:");
       ClusterStatus clusterStatus = SimpleGangliaClient.getClusterStatus(GANGLIA_GMETAD_HOST);
       System.out.println(clusterStatus);
    
-      System.out.println("Testing interactive port");
+      System.out.println("Testing interactive port:");
       clusterStatus = SimpleGangliaClient.getClusterStatus(GANGLIA_GMETAD_HOST,CLUSTER_NAME);
       System.out.println(clusterStatus);
+
+      System.out.println("Host List: ");
+      for (String hostname : clusterStatus.getAllHostNames()) {
+         System.out.println("    " + hostname);
+      }
+
+      System.out.println("Load Average: ");
+      for (HostStatus hostStatus : clusterStatus.getAllHostStatuses()) {
+         System.out.println("    " + hostStatus.getName() + " " + hostStatus.getMetric("load_one"));
+      }
 
    }
 }
